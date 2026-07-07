c4c7dfa03eef2aee19af6c0d3222dbdd882e42e3
jcruz-2021644 <jcruz-2021644@kinal.edu.gt>
2026-07-06 21:22:05 -0600

    Feat: se agregaron mas proyectos desplegados y se modifico el cv

--- src/data/projects.js
+++ src/data/projects.js
@@ -14,7 +14,40 @@ import agencia_viajes from '../assets/img/projects/agencia_viajes.png'
 import agencia_viajes2 from '../assets/img/projects/agencia_viajes2.png'
 import agencia_viajes3 from '../assets/img/projects/agencia_viajes3.png'
 
+//proyecto de lumina bank
+import lumina_bank from '../assets/img/projects/lumina_bank.png'
+import lumina_bank2 from '../assets/img/projects/lumina_bank2.png'
+import lumina_bank3 from '../assets/img/projects/lumina_bank3.png'
+
+//proyecto de kinal eats
+import restaurante from '../assets/img/projects/restaurante.png'
+import restaurante2 from '../assets/img/projects/restaurante2.png'
+import restaurante3 from '../assets/img/projects/restaurante3.png'
+
+//proyecto de kinal eats
 export const projects = [
+  {
+    title: 'Lumina Bank',
+    category: 'Aplicacion web',
+    description:
+      'Aplicación orientada a gestionar un banco con interfaz web y mobile para el usuario y el administrador del banco, con funcionalidades de transacciones, cuentas, reportes, etc.',
+    learned: 'Se fortelicieron mis habilidades en diseño de interfaces, manejo de datos y seguridad en aplicaciones web, junto con la logia detras del uso real de la app en el día a día de un banco.',
+    tags: ['Node.js', 'React', 'Tailwind CSS', 'MongoDB', 'PostgreSQL', '...'],
+    images: [lumina_bank, lumina_bank2, lumina_bank3],
+    link: 'https://grupo6-sistema-bancario-frontend-dun.vercel.app/',
+    repository: 'https://github.com/rcana-2021639/grupo6-sistema-bancario-Frontend',
+  },
+  {
+    title: 'Kinal Eats',
+    category: 'Aplicacion web',
+    description:
+      'Aplicación orientada al manejo de varios restaurantes con una interfaz en la web y mobile para el usuario y el administrador del restaurante, con funcionalidades de menú, pedidos, reportes, etc.',
+    learned: 'Se fortelicieron mis habilidades en diseño de interfaces, manejo de datos y seguridad en aplicaciones web, junto con la logia detras del uso real de la app en el día a día de un restaurante.',
+    tags: ['Node.js', 'React', 'Tailwind CSS', 'MongoDB', 'PostgreSQL', '...'],
+    images: [restaurante, restaurante2, restaurante3],
+    link: 'https://kinaleats.vercel.app/',
+    repository: 'https://github.com/rrodriguez-2023342/Proyecto_Restaurantes',
+  },
   {
     title: 'Agenda de Contactos',
     category: 'Aplicación web',
